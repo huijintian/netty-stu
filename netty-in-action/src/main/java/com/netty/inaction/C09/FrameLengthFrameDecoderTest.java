074968b350e60a56c2af2e478691fcd4f985fb11
mengtian <wu750316@163.com>
2018-11-21 23:36:19 +0800

    EmbeddedChannel测试

---
+++ netty-in-action/src/main/java/com/netty/inaction/C09/FrameLengthFrameDecoderTest.java
@@ -0,0 +1,29 @@
+package com.netty.inaction.C09;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mengtian on 2018/11/19
+ */
+public class FrameLengthFrameDecoderTest {
+
+    @Test
+    public void testFramesDecoded() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+        ByteBuf input = buf.duplicate();
+        EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+
+        assertTrue(channel.writeInbound(input.retain()));
+        assertTrue(channel.finish());
+
+
+    }
+}
