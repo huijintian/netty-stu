bcdad7d859c0cc81f9be1f3a1589959f1cf10123
mengtian <wu750316@163.com>
2018-11-26 21:55:17 +0800

    add C12 webSocket

---
+++ netty-in-action/src/main/java/com/netty/inaction/C12/webSocket/ChatServer.java
@@ -0,0 +1,63 @@
+package com.netty.inaction.C12.webSocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by mengtian on 2018/11/26
+ */
+public class ChatServer {
+    private final ChannelGroup channelGroup = new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
+    private final EventLoopGroup group = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture start(InetSocketAddress address) {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(createInitializer(channelGroup));
+        ChannelFuture future = bootstrap.bind(address);
+        future.syncUninterruptibly();
+        channel = future.channel();
+        return future;
+    }
+
+    private ChannelHandler createInitializer(ChannelGroup channelGroup) {
+        return new ChatServerInitializer(channelGroup);
+    }
+
+    public void destory() {
+        if (channel != null) {
+            channel.close();
+        }
+        channelGroup.close();
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please give port as argument");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        final ChatServer endPoint = new ChatServer();
+        ChannelFuture future = endPoint.start(new InetSocketAddress(port));
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                endPoint.destory();
+            }
+        });
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
